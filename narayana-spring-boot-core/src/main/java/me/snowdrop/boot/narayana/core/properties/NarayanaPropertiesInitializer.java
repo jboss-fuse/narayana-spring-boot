5ea3d5bd4292bad95c05278a39b801448c81b41c
Benjamin Graf <benjamin.graf@gmx.net>
2024-05-23 16:17:43 +0200

    Compute XA recovery nodes from node identifier by default

--- narayana-spring-boot-core/src/main/java/me/snowdrop/boot/narayana/core/properties/NarayanaPropertiesInitializer.java
+++ narayana-spring-boot-core/src/main/java/me/snowdrop/boot/narayana/core/properties/NarayanaPropertiesInitializer.java
@@ -83,6 +83,9 @@ public class NarayanaPropertiesInitializer implements InitializingBean {
     }
 
     private void setXARecoveryNodes(List<String> xaRecoveryNodes) {
+        if (xaRecoveryNodes.isEmpty()) {
+            xaRecoveryNodes = List.of(getPopulator(CoreEnvironmentBean.class).getNodeIdentifier());
+        }
         getPopulator(JTAEnvironmentBean.class).setXaRecoveryNodes(xaRecoveryNodes);
     }
 
